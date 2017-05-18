0ae277607a4655acb0928199d576e6abc9257033
thairyn <thrynhenderson@gmail.com>
2017-05-18 12:18:17 +0100

    variable platform spawn height

--- Assets/PlatformManager.cs
+++ Assets/PlatformManager.cs
@@ -7,10 +7,13 @@ public class PlatformManager : MonoBehaviour {
     public Transform platformprefab;
     public GameObject[] obj;
     private bool clearspace = true;
+    private Camera cam = Camera.main;
     Vector3 platpos;
 
-    public float MinSpawn;
-    public float MaxSpawn;
+    public float MinSpawnTime;
+    public float MaxSpawnTime;
+    public float MinVertSpread;
+    public float MaxVertSpread;
 
     void Start()
     {
@@ -20,23 +23,24 @@ public class PlatformManager : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other)
     {
         clearspace = false;
-        Debug.Log("no space bro");
+        //Debug.Log("no space bro");
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
         clearspace = true;
-        Debug.Log("okay go");
+        //Debug.Log("okay go");
     }
 
     void SpawnPlatforms()
     {
-        platpos = new Vector3(transform.position.x, transform.position.y, 0);
         if (clearspace)
         {
+            float platspace = Mathf.Clamp(Random.Range(MinVertSpread, MaxVertSpread), transform.position.y, Screen.height);
+            platpos = new Vector3(transform.position.x, platspace, 0);
             Instantiate(obj[Random.Range(0, obj.GetLength(0))], platpos, Quaternion.identity);
         }
-            Invoke("SpawnPlatforms", Random.Range(MinSpawn, MaxSpawn));
+            Invoke("SpawnPlatforms", Random.Range(MinSpawnTime, MaxSpawnTime));
 
     }
 
